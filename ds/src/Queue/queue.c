27c0906a2a8b998658cb1e8ceaf142ced5cb7e77
Anat.Wax <anatwax@gmail.com>
2020-03-03 18:18:04 +0200

    commited by alias

--- ds/src/Queue/queue.c
+++ ds/src/Queue/queue.c
@@ -1,11 +1,40 @@
-typedef struct queue_node
-{
-	void *data;
-	struct sll_node *next;
-}node_t;
+#include <stdlib>
 
-struct queue
+#include "queue.h"
+#include "../SinglyLinkedList/singly_linked_list.h"
+
+/*
+struct SLL
 {
-	node_t *front;
-	node_t *rear;
+	node_t *head;
+	node_t *tail;
 };
+*/
+
+typedef struct queue
+{
+	SLL_t *list;
+};
+
+queue_t *QueueCreate()
+{
+	SLL_t *queue_list = SLLCreate(); /*create a new list */
+	
+	if(NULL == queue_list)
+	{
+		return (NULL);
+	}
+	
+	SLL_t *queue = (SLL_t *)malloc(sizeof(SLL_t)); /* create a new queue */
+	
+	if(NULL == queue) 	/* if mallocs failed */
+	{
+		return (NULL);
+	}
+	SLL_t *queue_list
+	
+}
+
+
+/*int QueueEnqueu(queue_t *queue)
+Enqueue: Adds an item to the queue. If the queue is full, then it is said to be an Overflow condition.*/
