e9fc33ce1422c62aac5f627b1e258051bdfce49f
anat.wax <anatwax@gmail.com>
2020-03-03 22:31:04 +0200

    commited from home using alias

--- ds/src/Queue/queue.c
+++ ds/src/Queue/queue.c
@@ -1,3 +1,5 @@
+/* I'm using the struct of node (node_t) from the Singly Linked List exersize */
+
 #include <stdlib>
 
 #include "queue.h"
@@ -8,7 +10,7 @@ struct SLL
 {
 	node_t *head;
 	node_t *tail;
-};
+}SLL_t;
 */
 
 typedef struct queue
@@ -20,21 +22,26 @@ queue_t *QueueCreate()
 {
 	SLL_t *queue_list = SLLCreate(); /*create a new list */
 	
-	if(NULL == queue_list)
+	if(NULL == queue_list) /* if malloc failed */
 	{
 		return (NULL);
 	}
 	
-	SLL_t *queue = (SLL_t *)malloc(sizeof(SLL_t)); /* create a new queue */
+	queue_t *queue = (SLL_t *)malloc(sizeof(SLL_t)); /* create a new queue */
 	
-	if(NULL == queue) 	/* if mallocs failed */
+	if(NULL == queue) 	/* if malloc failed */
 	{
 		return (NULL);
 	}
 	SLL_t *queue_list
 	
+	
+	
 }
 
 
-/*int QueueEnqueu(queue_t *queue)
-Enqueue: Adds an item to the queue. If the queue is full, then it is said to be an Overflow condition.*/
+int QueueEnqueu(queue_t *queue)
+{
+	node
+}
+/*Enqueue: Adds an item to the queue. If the queue is full, then it is said to be an Overflow condition.*/
