8358c4ec5621091b5cf08bb298511274e4215cec
anat.wax <anatwax@gmail.com>
2020-05-02 21:40:22 +0300

    commited from home using alias

---
+++ ds/test/final_ds_April.c
@@ -0,0 +1,29 @@
+    #include <stddef.h> /* offsetof(), size_t */
+    #include <stdlib.h> /* malloc(), free(), abs() */ //* size_t, *//
+    #include <assert.h> /* assert() */
+    #include <time.h> /* time, size_t, srand() */
+    #include <unistd.h> /* ssize_t, sleep(), execvp(), fork() */
+    #include <stdio.h> /* printf(), size_t */
+    #include <string.h> /* size_t, atoi() */
+    #include <sys/types.h> /* pid_t */
+    #include <sys/wait.h> /* wait() */
+    #include <pthread.h> /* pthread_t, pthread_create() */
+
+#include "utility.h"
+#include "singly_linked_list.h"
+
+int main()
+{
+    
+    Flip;
+
+    return (0);
+}
+
+Node_t *Flip(node *node)
+{
+    
+    {
+        node
+    }
+}
