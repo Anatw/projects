63c2b3447d1276fb6b0e8ac3825971b7a4b2c25c
anat.wax <anatwax@gmail.com>
2020-06-17 15:52:13 +0300

    commited from home using alias

--- cpp/src/CPP2C/cpp2csrc.c
+++ cpp/src/CPP2C/cpp2csrc.c
@@ -16,7 +16,6 @@ Created on: June 10th, 2020
 Reviewer: Lior Cohen
 *******************************************************************************/
 #include <stdio.h>  /* printf(), size_t */
-#include <string.h> /* size_t, atoi(), memset() */
 #include <stdlib.h> /* malloc(), free(), abs(), size_t */
 
 #include "cpp2csrc.h"
