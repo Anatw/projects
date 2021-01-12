d2cf1541a0faa4206775f283b441021f25cdae3b
anat.wax <anatwax@gmail.com>
2021-01-12 23:16:28 +0200

    bank commit

--- system_programming/src/TCP-UDP/UDP_client.c
+++ system_programming/src/TCP-UDP/UDP_client.c
@@ -1,6 +1,4 @@
 /*******************************************************************************
-Comment and un-comment the defines to see both phases (one at a time).
-
 compile with GCC (and not gd! - getaddrinfo() is not competible for c99).
 
 TCP/UDP PingPong (Exercise 1)
@@ -9,89 +7,52 @@ Created: 18.6.20
 Reviewer: Yaacov Evenchik
 *******************************************************************************/
 #include <stdio.h>      /* printf(), size_t */
-#include <stdlib.h>     /* malloc(), free(), abs(), size_t, EXIT_STATUS */
-#include <string.h>     /* size_t, atoi(), memset() */
+#include <stdlib.h>     /* EXIT_STATUS */
+#include <string.h>     /* memset(), strlen() */
 #include <sys/wait.h>   /* wait() */
 #include <sys/types.h>  /* getaddrinfo() */
-#include <sys/socket.h> /* getaddrinfo() */
+#include <sys/socket.h> /* socket API functions */
 #include <netdb.h>      /* getaddrinfo() */
 #include <unistd.h>     /* close() */
 
 #include <netinet/in.h>
 
-#define PORT ("8002")
+#define PORT (9002)
 #define BUF_SIZE (256)
+#define NUM_OF_PORTS (5)
 
 int main()
 {
-    int socket_fd;
-    char buffer[BUF_SIZE];
-    struct addrinfo hints, *server_info, *i;
-    struct sockaddr_storage their_addr;
-    int retval;
+    int sockfd = 0;
+    struct sockaddr_in server_address;
+    socklen_t client_length = 0;
+    int bytes_recv = 0;
+    char buffer[BUF_SIZE] = {0};
 
-    int num_of_bytes_received = 0;
-    socklen_t addr_len = sizeof(their_addr);
+    char *massage = "This massage was sent from the client process";
 
-    printf("server loading...\n");
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET; /* AF_INET will use IPv4 protocol */
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE; /* use my IP */
-
-    if ((retval = getaddrinfo(NULL, PORT, &hints, &server_info)) != 0)
+    if ((-1) == (sockfd = socket(AF_INET, SOCK_DGRAM, 0)))
     {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(retval));
+        perror("ERROR in socket creation\n");
         exit(EXIT_FAILURE);
     }
 
-    /* loop through all the results and bind to the first we can */
-    for (i = server_info; i != NULL; i = i->ai_next)
-    {
-        if ((socket_fd = socket(i->ai_family, i->ai_socktype,
-                                i->ai_protocol)) == -1)
-        {
-            perror("server: socket");
-            continue;
-        }
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
-        if (bind(socket_fd, i->ai_addr, i->ai_addrlen) == -1)
-        {
-            close(socket_fd);
-            perror("server: bind");
-            continue;
-        }
+    sendto(sockfd, massage, strlen(massage), MSG_CONFIRM,
+           (const struct sockaddr *)&server_address, sizeof(server_address));
 
-        break;
-    }
+    bytes_recv = recvfrom(sockfd, buffer, BUF_SIZE, MSG_WAITALL,
+                          (struct sockaddr *)&server_address, &client_length);
 
-    if (i == NULL)
-    {
-        fprintf(stderr, "ERROR in server: failed to bind socket\n");
-        exit(EXIT_FAILURE);
-    }
+    buffer[bytes_recv] = '\0';
 
-    freeaddrinfo(server_info);
+    printf("massage received from server:\n%s", buffer);
 
-    while (1)
-    {
-        printf("server is listening...\n");
+    close(sockfd);
 
-        if ((num_of_bytes_received = recvfrom(socket_fd,
-                                              buffer,
-                                              BUF_SIZE - 1,
-                                              0,
-                                              (struct sockaddr *)&their_addr,
-                                              &addr_len)) == -1)
-        {
-            perror("ERROR in recvfrom");
-            exit(EXIT_FAILURE);
-        }
-
-        buffer[num_of_bytes_received] = '\0';
-        printf("server: packet contains \"%s\"\n", buffer);
-    }
-
-    close(socket_fd);
+    return (0);
 }
