5f52cada824fdb402d6316388eae8b5a816ea24a
anat.wax <anatwax@gmail.com>
2021-01-18 12:58:57 +0200

    commited using alias

--- Django/employee_microservice/employee_register/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Employee(models.Model):
-    fullname = models.CharField(max_length=35, unique=True)
-    country = models.CharField(max_length=20)
-    city = models.CharField(max_length=20)
-    salary = models.IntegerField()
--- Django/employee_microservice/selen_tes.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path="/home/anat/anat-wax/Django/selenium/chromedriver_linux64/chromedriver")
-driver.get("http://google.com")
-driver.close()
